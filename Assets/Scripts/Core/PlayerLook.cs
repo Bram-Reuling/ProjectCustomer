5fb51e5f214cbe8686af47268e191dbf7619bc40
Bram Reuling <bram.reuling@outlook.com>
2020-08-31 13:04:32 +0200

    Added Player
    Added player and player controls to the game.
    
    Player can jump and walk around.

---
+++ Assets/Scripts/Core/PlayerLook.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+
+namespace TimeLess.Core
+{
+    public class PlayerLook : MonoBehaviour
+    {
+        #region Fields
+
+        public Transform player;
+        [Range(1, 1000)] public int mouseSensitivity = 100;
+
+        private float xRotation = 0f;
+
+        #endregion
+
+        #region Start Up
+
+        private void Start()
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        #endregion
+
+        #region Update
+
+        private void Update()
+        {
+            var mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+            var mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+            xRotation -= mouseY;
+
+            xRotation = Mathf.Clamp(xRotation, -90, 90);
+            
+            transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+            player.Rotate(Vector3.up * mouseX);
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/Scripts/Core/PlayerMovement.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using UnityEngine;
+
+namespace TimeLess.Core
+{
+    [RequireComponent(typeof(CharacterController))]
+    public class PlayerMovement : MonoBehaviour
+    {
+        #region Fields
+
+        private CharacterController controller;
+
+        public float speed = 12f;
+        public float gravity = -9.81f;
+        public float jumpHeight = 3f;
+
+        public Transform groundCheck;
+        public float groundDistance = 0.4f;
+        public LayerMask groundMask;
+        
+        private Vector3 velocity;
+        private bool isGrounded;
+
+        #endregion
+
+        #region Start Up
+
+        private void Awake()
+        {
+            controller = GetComponent<CharacterController>();
+        }
+
+        #endregion
+
+        #region Updates
+
+        private void Update()
+        {
+            GroundCheck();
+
+            MovePlayer();
+
+            Jump();
+            
+            ApplyGravity();
+        }
+
+        #endregion
+
+        #region Movement
+
+        private void Jump()
+        {
+            if (Input.GetButtonDown("Jump") && isGrounded)
+            {
+                velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            }
+        }
+
+        private void ApplyGravity()
+        {
+            velocity.y += gravity * Time.deltaTime;
+
+            controller.Move(velocity * Time.deltaTime);
+        }
+
+        private void GroundCheck()
+        {
+            isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+            if (isGrounded && velocity.y < 0)
+            {
+                velocity.y = -2f;
+            }
+        }
+
+        private void MovePlayer()
+        {
+            var x = Input.GetAxis("Horizontal");
+            var z = Input.GetAxis("Vertical");
+
+            var move = transform.right * x + transform.forward * z;
+            move = Vector3.ClampMagnitude(move, 1f);
+            
+            controller.Move(move * (speed * Time.deltaTime));
+        }
+
+        #endregion
+    }
+}
