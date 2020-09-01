53007c7bf13e7fed34cae6117f0485ff01327614
Bram Reuling <bram.reuling@outlook.com>
2020-09-01 20:52:08 +0200

    Added simple waypoint system
    - Need to expand it further to make it suitable for multiple waypoints and events

--- Assets/Scripts/Waypoint/MissionWaypoint.cs
+++ Assets/Scripts/Waypoint/MissionWaypoint.cs
@@ -1,19 +1,41 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace ProjectCustomer.Waypoint
 {
     public class MissionWaypoint : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
+        #region Fields
+
+        public RawImage marker;
+        public Transform target;
+
+        #endregion
+
+        #region Update
+
+        private void Update()
         {
-        
+            var minX = marker.GetPixelAdjustedRect().width / 2;
+            var maxX = Screen.width - minX;
+
+            var minY = marker.GetPixelAdjustedRect().height / 2;
+            var maxY = Screen.height - minY;
+            
+            var position = Camera.main.WorldToScreenPoint(target.position);
+
+            if (Vector3.Dot((target.position - transform.position), transform.forward) < 0)
+            {
+                position.x = position.x < Screen.width / 2 ? maxX : minX;
+            }
+
+            position.x = Mathf.Clamp(position.x, minX, maxX);
+            position.y = Mathf.Clamp(position.y, minY, maxY);
+
+            marker.transform.position = position;
         }
 
-        // Update is called once per frame
-        void Update()
-        {
-        
-        }
+        #endregion
     }
 }
