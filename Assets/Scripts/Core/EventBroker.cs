90aef29dbaf0461af2f6728b4f384bd254019e39
Bram Reuling <bram.reuling@outlook.com>
2020-09-02 09:33:24 +0200

    AddedEventSystem

---
+++ Assets/Scripts/Core/EventBroker.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ProjectCustomer.Core
+{
+    public static class EventBroker
+    {
+        public static Action EventOnBinocular;
+
+        public static void CallEventOnBinocular()
+        {
+            EventOnBinocular?.Invoke();
+        }
+    }
+}
