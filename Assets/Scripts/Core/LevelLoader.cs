864b34f40bac157be63d268e3ecc5ec18ca87c16
Bram Reuling <bram.reuling@outlook.com>
2020-09-09 12:00:32 +0200

    Added quick main menu

---
+++ Assets/Scripts/Core/LevelLoader.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace ProjectCustomer.Core
+{
+    public class LevelLoader : MonoBehaviour
+    {
+        public void LoadNewScene(int scene)
+        {
+            SceneManager.LoadScene(scene);
+        }
+    }
+}
