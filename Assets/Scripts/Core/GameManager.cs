7c75509a8eb21407c197c540434cab7ee109d9db
Bram Reuling <bram.reuling@outlook.com>
2020-09-17 13:26:56 +0200

    Added Win/Loss

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -8,32 +8,58 @@ namespace ProjectCustomer.Core
         public TextMeshProUGUI currentNumberOfFiresText;
         public int numberOfFiresToEndGame = 4;
         private int currentNumberOfFires;
+        private LevelLoader levelLoader;
+        public int sceneIdToSwitchTo;
 
         private void Start()
         {
             EventBroker.EventOnFireStarted += CurrentFireIncrease;
             EventBroker.EventOnFireExtinguished += CurrentFireDecrease;
 
+            EventBroker.EventOnFoxRevive += FoxesReviveIncrease;
+
+            EventBroker.EventOnTimerRunOut += TimerRunOut;
+
             currentNumberOfFires = 0;
+
+            levelLoader = GetComponent<LevelLoader>();
         }
 
         private void Update()
         {
             currentNumberOfFiresText.text = $"Current Number of Fires: {currentNumberOfFires}";
+
+            DataHandler.numberOfFoxesInScene = GameObject.FindGameObjectsWithTag("Fox").Length;
             
             if (currentNumberOfFires != numberOfFiresToEndGame) return;
             //Debug.Log("MAX FIRES! GAME OVER!");
-            EventBroker.CallEventOnMaxFires();
+            //EventBroker.CallEventOnMaxFires();
+            DataHandler.playerLost = true;
+            levelLoader.LoadNewScene(sceneIdToSwitchTo);
         }
 
+        private void FoxesReviveIncrease()
+        {
+            DataHandler.numberOfFoxesRevived++;
+        }
+
+        private void TimerRunOut()
+        {
+            DataHandler.playerLost = false;
+            levelLoader.LoadNewScene(sceneIdToSwitchTo);
+        }
+        
         private void CurrentFireIncrease()
         {
             currentNumberOfFires++;
+            DataHandler.numberOfFiresLeft++;
         }
 
         private void CurrentFireDecrease()
         {
             currentNumberOfFires--;
+            DataHandler.numberOfFiresLeft--;
+            DataHandler.numberOfFiresPutOut++;
         }
     }
 }
