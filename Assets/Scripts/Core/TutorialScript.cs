40df80da4393169748c43c976d267585967fbecb
vipulul <gamer.europa2@gmail.com>
2020-09-17 14:25:22 +0300

    Almost Finished Main Menu

--- Assets/Scripts/Core/TutorialScript.cs
+++ Assets/Scripts/Core/TutorialScript.cs
@@ -7,11 +7,12 @@ namespace ProjectCustomer.Core
 {
     public class TutorialScript : MonoBehaviour
     {
-        public GameObject[] waypoints,canvas;
+        public GameObject[] waypoints, canvas;
+        public GameObject MainUI;
         public Transform ogtransofrm;
-        public float cammovespeed = 5f;
+        public float cammovespeed;
 
-        public bool bin = false, camp = false, lake = false, fox = false;
+        public bool bin = false, camp = false, lake = false, fox = false, last = false;
 
         // Start is called before the first frame update
         void Start()
@@ -22,14 +23,15 @@ namespace ProjectCustomer.Core
             canvas[1].SetActive(false);
             canvas[2].SetActive(false);
             canvas[3].SetActive(false);
+            canvas[4].SetActive(false);
 
-        
+
         }
 
         // Update is called once per frame
         void Update()
         {
-            if(bin)
+            if (bin)
             {
                 Camera.main.transform.position = Vector3.Slerp(Camera.main.transform.position, waypoints[0].transform.position, cammovespeed * Time.deltaTime);
                 Camera.main.transform.rotation = Quaternion.Slerp(Camera.main.transform.rotation, waypoints[0].transform.rotation, cammovespeed * Time.deltaTime);
@@ -49,6 +51,11 @@ namespace ProjectCustomer.Core
                 Camera.main.transform.position = Vector3.Slerp(Camera.main.transform.position, waypoints[3].transform.position, cammovespeed * Time.deltaTime);
                 Camera.main.transform.rotation = Quaternion.Slerp(Camera.main.transform.rotation, waypoints[3].transform.rotation, cammovespeed * Time.deltaTime);
             }
+            else if (last)
+            {
+                Camera.main.transform.position = Vector3.Slerp(Camera.main.transform.position, waypoints[4].transform.position, cammovespeed * Time.deltaTime);
+                Camera.main.transform.rotation = Quaternion.Slerp(Camera.main.transform.rotation, waypoints[4].transform.rotation, cammovespeed * Time.deltaTime);
+            }
             else
             {
                 Camera.main.transform.position = Vector3.Slerp(Camera.main.transform.position, ogtransofrm.transform.position, cammovespeed * Time.deltaTime);
@@ -56,8 +63,25 @@ namespace ProjectCustomer.Core
             }
         }
 
+        public void endTut()
+        {
+            MainUI.SetActive(true);
+            canvas[0].SetActive(false);
+            canvas[1].SetActive(false);
+            canvas[2].SetActive(false);
+            canvas[3].SetActive(false);
+            canvas[4].SetActive(false);
+
+        }
+
         public void startTut()
         {
+            MainUI.SetActive(false);
+            canvas[0].SetActive(false);
+            canvas[1].SetActive(false);
+            canvas[2].SetActive(false);
+            canvas[3].SetActive(false);
+            canvas[4].SetActive(false);
             StartCoroutine(Delay());
         }
 
@@ -67,10 +91,12 @@ namespace ProjectCustomer.Core
             camp = false;
             lake = false;
             fox = false;
+            last = false;
             canvas[0].SetActive(true);
             canvas[1].SetActive(false);
             canvas[2].SetActive(false);
             canvas[3].SetActive(false);
+            canvas[4].SetActive(false);
         }
 
         void Cmapfire()
@@ -79,10 +105,12 @@ namespace ProjectCustomer.Core
             camp = true;
             lake = false;
             fox = false;
+            last = false;
             canvas[0].SetActive(false);
             canvas[1].SetActive(true);
             canvas[2].SetActive(false);
             canvas[3].SetActive(false);
+            canvas[4].SetActive(false);
         }
 
         void Lake()
@@ -91,10 +119,12 @@ namespace ProjectCustomer.Core
             camp = false;
             lake = true;
             fox = false;
+            last = false;
             canvas[0].SetActive(false);
             canvas[1].SetActive(false);
             canvas[2].SetActive(true);
             canvas[3].SetActive(false);
+            canvas[4].SetActive(false);
         }
 
         void Fox()
@@ -103,21 +133,42 @@ namespace ProjectCustomer.Core
             camp = false;
             lake = false;
             fox = true;
+            last = false;
             canvas[0].SetActive(false);
             canvas[1].SetActive(false);
             canvas[2].SetActive(false);
             canvas[3].SetActive(true);
+            canvas[4].SetActive(false);
+        }
+
+        void Last()
+        {
+            bin = false;
+            camp = false;
+            lake = false;
+            fox = false;
+            last = true;
+            canvas[0].SetActive(false);
+            canvas[1].SetActive(false);
+            canvas[2].SetActive(false);
+            canvas[3].SetActive(false);
+            canvas[4].SetActive(true);
         }
 
         IEnumerator Delay()
         {
+
             Binocular();
-            yield return new WaitForSeconds(5f);
+            yield return new WaitForSeconds(7f);
+            cammovespeed = cammovespeed / 2;
             Cmapfire();
-            yield return new WaitForSeconds(5f);
+            yield return new WaitForSeconds(16f);
+            cammovespeed = cammovespeed * 2;
             Lake();
-            yield return new WaitForSeconds(5f);
+            yield return new WaitForSeconds(10f);
             Fox();
+            yield return new WaitForSeconds(8f);
+            Last();
         }
     }
 
